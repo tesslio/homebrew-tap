553d05b1118ab40e6193e967cb2afbe6d34e770f
Colin Bendell <colin@bendell.ca>
2025-10-31 19:56:54 +0100

    Add fixed version

---
+++ Formula/tessl@0.28.rb
@@ -0,0 +1,10 @@
+require_relative "./tessl.rb"
+class TesslAT028 < Tessl
+  desc "CLI and MCP to provide coding agents the context they are missing"
+  homepage "https://tessl.io"
+  url "https://registry.npmjs.org/@tessl/cli/-/cli-0.28.0.tgz"
+  sha256 "d6bc590511b83fc058af87e542e2c721d3ea16ceb907ebea6d89d53c44413800"
+  license ""
+
+  depends_on "node"
+end

2145925bb47c4c999e290ae34d477809e3600f7e
Colin Bendell <colin@tessl.io>
2025-11-14 12:32:31 -0500

    Ceanup formula (#15)

--- Formula/tessl-beta.rb
+++ Formula/tessl-beta.rb
@@ -3,14 +3,14 @@ class TesslBeta < Tessl
   desc "CLI and MCP to provide coding agents the context they are missing"
   homepage "https://tessl.io"
   url "https://tileworks-cli-assets.s3.amazonaws.com/releases/0.50.0-beta.2.tgz"
-  sha256 "eaf716fe107ed23c6e4a5cf2a65a4ccbd9f4d84e30b52c3fcaa622b89b12c813  -"
+  sha256 "eaf716fe107ed23c6e4a5cf2a65a4ccbd9f4d84e30b52c3fcaa622b89b12c813"
   license ""
 
   depends_on "node"
 
   option "with-version", "Install the test release"
   if build.with?("version") && ENV["HOMEBREW_TESSL_VERSION"]
-    url "https://tileworks-cli-assets.s3.amazonaws.com/releases/0.50.0-beta.2.tgz"
-    sha256 "eaf716fe107ed23c6e4a5cf2a65a4ccbd9f4d84e30b52c3fcaa622b89b12c813  -"
+    url "https://install.tessl.io/releases/#{ENV["HOMEBREW_TESSL_VERSION"].sub("+", "%2B")}.tgz"
+    sha256 ""
   end
 end

1e121e90a63916a9432f645fa692ce29e49e89b5
Shaun Smith <shaun@tessl.io>
2025-11-19 15:06:36 +0000

    chore: bump tessl to 0.50.6-beta.2

--- Formula/tessl-beta.rb
+++ Formula/tessl-beta.rb
@@ -2,8 +2,8 @@ require_relative "./tessl.rb"
 class TesslBeta < Tessl
   desc "CLI and MCP to provide coding agents the context they are missing"
   homepage "https://tessl.io"
-  url "https://install.tessl.io/releases/0.50.3-beta.1.tgz"
-  sha256 "7adbeb33fb562f0480f77df24d51b55fb9535a8e514e1ddcf35fad9567b5de73"
+  url "https://install.tessl.io/releases/0.50.6-beta.2.tgz"
+  sha256 "6e3b466f652c141c80c972ffdcb42ea456fd43be17e4e7f7980ee677c7d74df6"
   license ""
 
   depends_on "node"

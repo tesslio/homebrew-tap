e7be07340cef723d4a1b7699ecde8b2976c9ae3e
robw-tessl <robw@tessl.io>
2025-12-10 17:00:36 +0000

    fix: support native binding addons (#24)

--- Formula/tessl.rb
+++ Formula/tessl.rb
@@ -11,7 +11,7 @@ class Tessl < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *std_npm_args
+    system "npm", "install", *std_npm_args(ignore_scripts: false)
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
